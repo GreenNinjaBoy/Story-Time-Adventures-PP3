f3c65225b9dd323bd9be0bb32c08c35639bef598
J.Connell1990 <68825060+GreenNinjaBoy@users.noreply.github.com>
2023-10-19 23:09:54 +0000

    (run.py): defined a function that when called changes text speed

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print("Inital Commit")
+import time
+
+
+def print_slow(str):
+    """
+    Creating a function that when called,
+    will print text slower to the terminal.
+    """
+    for letter in str:
+        print(letter, end='', flush=True)
+        time.sleep(0.05)
+    print()
+
+print_slow("Testing Print speed")
+print("Normal Speed")
