cdde049942316f467072385be3906b5ef59a430b
J.Connell1990 <68825060+GreenNinjaBoy@users.noreply.github.com>
2023-10-20 20:59:50 +0000

    (run.py): add main header using pyfiglet

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import time
+from termcolor import colored, cprint
+from pyfiglet import Figlet
 
 
 def print_slow(str):
@@ -17,7 +19,9 @@ def intro_to_game():
     Creating a function which gives the user
     a brief introduction 
     """
-    print_slow("Welcome to Story Time Adventures")
+    f = Figlet(font='slant')
+    print(colored(f.renderText('Story Time Adventure'), color="green"))
+    print_slow(colored('Welcome to Story Time Adventures', color="red"))
     print_slow("Where you choices help tell the story")
     print_slow("You can get a grown up to help you on your adventure")
 
@@ -31,10 +35,17 @@ def get_story_tellers_name():
 
 def choosing_a_story(player_name):
     print_slow("Its time to pick a story")
-    print_slow(f"Now, {player_name} please choose")
     print_slow(f"1. {player_name} saves their magical unicorn friend")
     print_slow(f"2. {player_name}'s big park adventure")
-
+    print_slow(f"Now, {player_name} please choose")
+    choice = input()
+    if choice == "1":
+        print_slow(f"{player_name} saves their magical unicorn friend")
+    elif choice == "2":
+        print_slow(f"{player_name}'s big park adventure")
+    else:
+        print_slow("I am sorry I didn't understand please choose either '1' or '2'")
+        choosing_a_story(player_name)
 
 def main():
     intro_to_game()
