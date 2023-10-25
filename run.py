178ceb93804b581fdee632bc64f073d67cd526a3
GreenNinjaBoy <>
2023-10-25 11:04:40 +0000

    (run.py) add more content to first story with first set of choices

--- run.py
+++ run.py
@@ -31,39 +31,61 @@ def load_adventure_stories(file_path):
         return adventures
 
 
-def choose_path(player_name, adventures):
-    print_slow(f"Hello {player_name}! Once upon a time you find yourself in a magic forest ")
+def choose_path(player_name, adventures, princess_story):
+    print_slow(f"Hello {player_name}! You wake up in a magic forest ")
     print_slow("Before you there are two paths 'left' and 'right'.")
     print_slow("The left path will take you on an adventure to save a princess.")
-    print_slow("The right path will take you an a space adventure.")
-    print_slow("which path would you like to take?")
+    print_slow("The right path will take you on a space adventure.")
+    print_slow("Which path would you like to take?")
     choice = input().lower()
     if choice == "left":
         print_slow(adventures[0])
-        from my_images import squirel_img
-        princess_adventure(player_name)
+        princess_adventure(princess_story)
     elif choice == "right":
         print_slow(adventures[1])
-        from my_images import portal_img
+        # Handle space adventure logic here
     else:
         print_slow("I am sorry I didn't understand")
-        print_slow("please enter either 'left' or 'right'")
-        choose_path(player_name, adventures)
+        print_slow("Please enter either 'left' or 'right'")
+        choose_path(player_name, adventures, princess_story)
 
 
 def main():
     intro_to_game()
     player_name = get_story_tellers_name()
     adventures = load_adventure_stories('story_adventure_1.txt')
-    choose_path(player_name, adventures)
+    princess_story = load_princess_story('princess_story.txt')
+    choose_path(player_name, adventures, princess_story)
 
-def princess_adventure(player_name):
-    print_slow("Once upon a time, in a faraway kingdom, there lived a beautiful princess named Isabella.")
-    print_slow("One fateful night, an evil sorcerer named Malachar cast a dark spell upon the kingdom.")
-    print_slow("causing Princess Isabella to disappear without a trace.")
-    print_slow("The entire kingdom fell into despair.")
-    print_slow(f"and it was believed that only a brave and noble soul called {player_name} could rescue the princess")
-    print_slow("and break the curse.")
+
+def load_princess_story(file_path):
+    with open(file_path, 'r') as file:
+        princess_story = file.readlines()
+        return princess_story
+
+
+def princess_adventure(princess_story):
+    for i in range(9):
+        print_slow(princess_story[i])
+
+    print_slow("1. Follow the Forest Trail:")
+    print_slow("2. Investigate the Abandoned Castle:")
+    print_slow("3. Seek the Guidance of the Wise Sage:")
+    print_slow("please choose a path")
+    try:
+        choice = int(input())
+        if choice == 1:
+            print_slow(princess_story[9])
+        elif choice == 2:
+            print_slow(princess_story[10])
+        elif choice == 3:
+            print_slow(princess_story[12])
+        else:
+            print_slow("I am sorry, please choose 1, 2, or 3")
+            princess_adventure(princess_story)  # Recursively call the function to prompt the user again
+    except ValueError:
+        print_slow("Invalid input. Please enter a number.")
+        princess_adventure(princess_story)
 
 
 if __name__ == "__main__":
