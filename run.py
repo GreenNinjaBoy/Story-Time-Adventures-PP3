a3c795c3cf90ab43890abe7d0668a8ffa8d2c5fa
GreenNinjaBoy <>
2023-10-26 12:38:42 +0000

    trying to simplify my code as it was a mess

--- run.py
+++ run.py
@@ -28,26 +28,24 @@ def get_story_tellers_name():
 def load_adventure_stories(file_path):
     with open(file_path, 'r') as file:
         adventures = file.readlines()
-        return adventures
+    return adventures
 
 
 def choose_path(player_name, adventures, princess_story):
-    print_slow(f"Hello {player_name}! You wake up in a magic forest ")
-    print_slow("Before you there are two paths 'left' and 'right'.")
-    print_slow("The left path will take you on an adventure to save a princess.")
-    print_slow("The right path will take you on a space adventure.")
-    print_slow("Which path would you like to take?")
-    choice = input().lower()
-    if choice == "left":
-        print_slow(adventures[0])
-        princess_adventure(princess_story)
-    elif choice == "right":
-        print_slow(adventures[1])
-        # Handle space adventure logic here
-    else:
-        print_slow("I am sorry I didn't understand")
-        print_slow("Please enter either 'left' or 'right'")
-        choose_path(player_name, adventures, princess_story)
+    print_slow(adventures[0:4])
+    while True:
+        choice = input("Which path would you like to take? ").lower()
+        if choice == "left":
+            print_slow(adventures[5])
+            princess_adventure(princess_story)
+            break
+        elif choice == "right":
+            print_slow(adventures[6])
+            # Handle space adventure logic here
+            break
+        else:
+            print_slow("I am sorry I didn't understand")
+            print_slow("Please enter either 'left' or 'right'")
 
 
 def main():
@@ -61,31 +59,64 @@ def main():
 def load_princess_story(file_path):
     with open(file_path, 'r') as file:
         princess_story = file.readlines()
-        return princess_story
+    return princess_story
 
 
 def princess_adventure(princess_story):
-    for i in range(9):
-        print_slow(princess_story[i])
+    print_slow('\n'.join(princess_story[:9]))
 
-    print_slow("1. Follow the Forest Trail:")
-    print_slow("2. Investigate the Abandoned Castle:")
-    print_slow("3. Seek the Guidance of the Wise Sage:")
-    print_slow("please choose a path")
-    try:
-        choice = int(input())
-        if choice == 1:
-            print_slow(princess_story[9])
-        elif choice == 2:
-            print_slow(princess_story[10])
-        elif choice == 3:
-            print_slow(princess_story[12])
+    paths = [
+        "Follow the Forest Trail",
+        "Investigate the Abandoned Castle",
+        "Seek the Guidance of the Wise Sage"
+    ]
+
+    print_slow("\n".join(f"{i + 1}. {path}" for i, path in enumerate(paths)))
+    print_slow("Please choose a path")
+
+    while True:
+        try:
+            choice = int(input())
+            if 1 <= choice <= 3:
+                break
+            else:
+                print_slow("I am sorry, please choose 1, 2, or 3")
+        except ValueError:
+            print_slow("Invalid input. Please enter a number.")
+
+    if choice == 1:
+        print_slow(princess_story[9])
+        princess_adventure_2(princess_story)
+    elif choice == 2:
+        print_slow(princess_story[14])
+    elif choice == 3:
+        print_slow(princess_story[15])
+
+
+def princess_adventure_2(princess_story):
+    print_slow(princess_story[10])
+
+    options = [
+        "Trust Squeaky and Follow His Advice",
+        "Ignore Squeaky and Continue on Your Own"
+    ]
+
+    print_slow("\n".join(f"{i + 1}. {option}" for i, option in enumerate(options)))
+    print_slow("What would you like to do?")
+
+    while True:
+        try:
+            choice = int(input())
+            if choice == 1:
+                print_slow(princess_story[11])
+            elif choice == 2:
+                print_slow(princess_story[12])
+            else:
+                print_slow("I am sorry, please choose 1 or 2")
+        except ValueError:
+            print_slow("Invalid input. Please enter a number.")
         else:
-            print_slow("I am sorry, please choose 1, 2, or 3")
-            princess_adventure(princess_story)  # Recursively call the function to prompt the user again
-    except ValueError:
-        print_slow("Invalid input. Please enter a number.")
-        princess_adventure(princess_story)
+            break
 
 
 if __name__ == "__main__":
