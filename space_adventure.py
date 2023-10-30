9eddaf7b3f0d756fe587cb2bbc9dfd725da46e54
GreenNinjaBoy <>
2023-10-30 10:32:56 +0000

    (space_adveture.py): added pages 8 and 10, and tested

--- space_adventure.py
+++ space_adventure.py
@@ -9,12 +9,22 @@ def load_space_adventure(file_path):
 space_adventure_file_path = 'space_adventure.txt'
 space_adventure = load_space_adventure(space_adventure_file_path)
 
+
+sa_page_10 = Page(
+    message="this is visit planet inhabitants story"
+)
+sa_page_8 = Page(
+    message="this is candy feast story"
+)
+
 sa_page_6 = Page(
     message="this is sparkling comets story"
 )
 
 sa_page_4 = Page(
-    message="This is Candy Planet Story"
+    message=space_adventure[16:20],
+    choices=["1. Do you want to have a candy feast?:", "2. Or do you want to visit the Candy Planet inhabitants?:"],
+    choices_mapping={1: sa_page_8, 2: sa_page_10}
 )
 
 sa_page_3 = Page(
@@ -23,12 +33,12 @@ sa_page_3 = Page(
 
 sa_page_2 = Page(
     message=space_adventure[11:15],
-    choices={"1. Do you want to land on the Candy Planet?:", "2. Do you want to chase the sparkling comets?:"},
+    choices=["1. Do you want to land on the Candy Planet?:", "2. Do you want to chase the sparkling comets?:"],
     choices_mapping={1: sa_page_4, 2: sa_page_6}
 )
 
 sa_page_1 = Page(
     message=space_adventure[0:9],
-    choices={"1. Would you like to explore the glittering Stardust Galaxy:", "2. or would you prefer to investigate the mysterious Nebula Nebula:"},
+    choices=["1. Would you like to explore the glittering Stardust Galaxy:", "2. or would you prefer to investigate the mysterious Nebula Nebula:"],
     choices_mapping={1: sa_page_2, 2: sa_page_3}
 )
