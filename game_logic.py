c6e8145146adc3372737ab8db97bd9f0497b29e9
J.Connell1990 <68825060+GreenNinjaBoy@users.noreply.github.com>
2023-11-02 20:22:45 +0000

    (game_logic.py): fixed print issue with story text

--- game_logic.py
+++ game_logic.py
@@ -11,6 +11,15 @@ princess_story_file_path = 'princess_story.txt'
 princess_story = load_princess_story(princess_story_file_path)
 
 
+def load_space_adventure(file_path):
+    with open(file_path, 'r') as file:
+        space_adventure_data = file.read().splitlines()
+    return space_adventure_data
+
+space_adventure_file_path = 'space_adventure.txt'
+space_adventure = load_space_adventure(space_adventure_file_path)
+
+
 def print_slow(text):
     for letter in text:
         print(letter, end='', flush=True)
@@ -40,10 +49,36 @@ def get_yes_or_no_choice():
             print("Invalid input. Please enter 'y' or 'n'.")
 
 def tick(current_page, princess_story_data, space_adventure_data):
-    message = current_page.message
+    message_1 = current_page.message
     message_2 = current_page.message_2
 
-    player_choice = get_choice(current_page.choices)
+    if isinstance(message_1, list):
+        for line in message_1:
+            print_slow(line)
+            time.sleep(0.1)  # Add a slightly longer delay between lines for clarity
+    else:
+        print_slow(message_1)
+    
+    if isinstance(message_2, list):
+        for line in message_2:
+            print_slow(line)  # Add a slightly longer delay between lines for clarity
+    elif message_2:
+        print_slow(message_2)  # Print message_2 slowly
+
+    if current_page.choices is None:
+        print_slow("Thanks for playing!")
+        sys.exit()
+
+    if current_page.choices == ['end_game']:
+        end_game_message = ''.join(princess_story_data[110:121])
+        print_slow(end_game_message)
+        print_slow("Thanks for playing!")
+        sys.exit()
+
+    if current_page.choices == ['y', 'n']:
+        player_choice = get_yes_or_no_choice()
+    else:
+        player_choice = get_choice(current_page.choices)
 
     next_page = current_page.choices_mapping[player_choice]
     tick(next_page, princess_story_data, space_adventure_data)
